3eb7d196d57db317e74e52c70171ef95196dbcbf
HoangVanTrinh92 <hoang.van.trinh@framgia.com>
2015-06-25 17:00:15 +0700

    add-name-and-birthday-to-user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150622075348) do
+ActiveRecord::Schema.define(version: 20150625071857) do
 
   create_table "activities", force: :cascade do |t|
     t.integer  "action",      limit: 4
@@ -102,6 +102,8 @@ ActiveRecord::Schema.define(version: 20150622075348) do
     t.string   "role",                   limit: 255
     t.string   "language",               limit: 255
     t.string   "string",                 limit: 255
+    t.datetime "birthday"
+    t.string   "name",                   limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :user do
     sequence (:email) {|n| "trinh#{n}@gmail.com"}
+    sequence (:name) {|n| "trinh#{n}"}
     password "12345678"
     password_confirmation "12345678"
     role "normal"
